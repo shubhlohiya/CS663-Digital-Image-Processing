879ac2cc810f39190d9841bfee24741071c6bb71
Shubham Lohiya <shubhlohiya.14@gmail.com>
2020-10-18 17:52:42 +0530

    final tuning

--- assignment-3-segmentation/2/code/myMainScript.m
+++ assignment-3-segmentation/2/code/myMainScript.m
@@ -3,62 +3,70 @@
 
 %% Baboon
 tic;
-hc = 30;
+hc = 0.06;
 hs = 25;
-n = 20;
+n = 15;
 
 pic = imread('../data/baboonColor.png');
-result = myMeanShiftSegmentation(pic, hc, hs, n, 0, 1.2);
-plotresult(pic, result);
-
+result = myMeanShiftSegmentation(pic, hc, hs, n);
+[labels,centres] = imsegkmeans(result,20);
+clustered = label2rgb(labels,im2double(centres));
+plotresult(pic, result, clustered);
 toc;
 
 % *Used Parameters*
-% * Gaussian Kernel bandwidth for color features = 50 (For intensities between 0 and 255)
-% * Gaussian Kernel bandwidth for the spatial features = 200
-% * Number of iterations = 20
-% * Learning rate = 1.2 (halved after half iterations are completed for better results)
-% * Nearest neighbours (using knnsearch()) for gradient calculation = 400
-% (random subset of 100 from those used each iteration to speed up results)
-
-%% Flower
-tic;
-hc = 0.2;
-hs = 20;
-n = 10;
-
-pic = imread('../data/flower.jpg');
-result = myMeanShiftSegmentation(pic, hc, hs, n, 1, 1);
-plotresult(pic, result);
-toc;
-
-
-% *Used Parameters*
-% * Gaussian Kernel bandwidth for color features = 0.05 (For intensities scaled between 0 and 1)
-% * Gaussian Kernel bandwidth for the spatial features = 500
-% * Number of iterations = 10
-% * Learning rate = 1 (halved after half iterations are completed for better results)
-% * Nearest neighbours (using knnsearch()) for gradient calculation = 400
-% (random subset of 100 from those used each iteration to speed up results)
-
-%% Bird
-tic;
-hc = 50;
-hs = 25;
-n = 20;
-
-pic = imread('../data/bird.jpg');
-result = myMeanShiftSegmentation(pic, hc, hs, n, 1, 1);
-plotresult(pic, result);
-toc;
-
-
-% *Used Parameters*
-% * Gaussian Kernel bandwidth for color features = 0.1 (For intensities scaled between 0 and 1)
-% * Gaussian Kernel bandwidth for the spatial features = 200
+% * Gaussian Kernel bandwidth for color features = 0.06 (For intensities scaled between 0 and 1)
+% * Gaussian Kernel bandwidth for the spatial features = 25
 % * Number of iterations = 15
 % * Learning rate = 1 (halved after half iterations are completed for better results)
 % * Nearest neighbours (using knnsearch()) for gradient calculation = 400
 % (random subset of 100 from those used each iteration to speed up results)
+% * Number of colors in clustered result = 20
+
+%% Flower
+tic;
+hc = 0.08;
+hs = 25;
+n = 15;
+
+pic = imread('../data/flower.jpg');
+result = myMeanShiftSegmentation(pic, hc, hs, n);
+[labels,centres] = imsegkmeans(result,10);
+clustered = label2rgb(labels,im2double(centres));
+plotresult(pic, result, clustered);
+toc;
+
+
+% *Used Parameters*
+% * Gaussian Kernel bandwidth for color features = 0.08 (For intensities scaled between 0 and 1)
+% * Gaussian Kernel bandwidth for the spatial features = 25
+% * Number of iterations = 15
+% * Learning rate = 1 (halved after half iterations are completed for better results)
+% * Nearest neighbours (using knnsearch()) for gradient calculation = 400
+% (random subset of 100 from those used each iteration to speed up results)
+% * Number of colors in clustered result = 10
+
+%% Bird
+tic;
+hc = 0.12;
+hs = 30;
+n = 15;
+
+pic = imread('../data/bird.jpg');
+result = myMeanShiftSegmentation(pic, hc, hs, n);
+[labels,centres] = imsegkmeans(result,10);
+clustered = label2rgb(labels,im2double(centres));
+plotresult(pic, result, clustered);
+toc;
+
+
+% *Used Parameters*
+% * Gaussian Kernel bandwidth for color features = 0.12 (For intensities scaled between 0 and 1)
+% * Gaussian Kernel bandwidth for the spatial features = 30
+% * Number of iterations = 15
+% * Learning rate = 1 (halved after half iterations are completed for better results)
+% * Nearest neighbours (using knnsearch()) for gradient calculation = 400
+% (random subset of 100 from those used each iteration to speed up results)
+% * Number of colors in clustered result = 10
 
 %%
