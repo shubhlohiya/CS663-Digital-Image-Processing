8db4e486ea09bb7780bf082f33fedd6d347d874c
Shubham Lohiya <shubhlohiya.14@gmail.com>
2020-10-18 14:19:30 +0530

    modified q-2 code

--- assignment-3-segmentation/2/code/myMainScript.m
+++ assignment-3-segmentation/2/code/myMainScript.m
@@ -3,38 +3,38 @@
 
 %% Baboon
 tic;
-hc = 0.07;
-hs = 200;
-n = 10;
+hc = 30;
+hs = 25;
+n = 20;
 
-pic = double(imread('../data/baboonColor.png'));
-result = uint8(myMeanShiftSegmentation(pic/255, hc, hs, n)*255);
-plotresult(uint8(pic), result);
+pic = imread('../data/baboonColor.png');
+result = myMeanShiftSegmentation(pic, hc, hs, n, 0, 1.2);
+plotresult(pic, result);
 
 toc;
 
 % *Used Parameters*
-% * Gaussian Kernel bandwidth for color features = 0.07 (For intensities scaled between 0 and 1)
+% * Gaussian Kernel bandwidth for color features = 50 (For intensities between 0 and 255)
 % * Gaussian Kernel bandwidth for the spatial features = 200
-% * Number of iterations = 10
-% * Learning rate = 1 (halved after half iterations are completed for better results)
+% * Number of iterations = 20
+% * Learning rate = 1.2 (halved after half iterations are completed for better results)
 % * Nearest neighbours (using knnsearch()) for gradient calculation = 400
 % (random subset of 100 from those used each iteration to speed up results)
 
 %% Flower
 tic;
-hc = 0.06;
-hs = 500;
+hc = 0.2;
+hs = 20;
 n = 10;
 
-pic = double(imread('../data/flower.jpg'));
-result = uint8(myMeanShiftSegmentation(pic/255, hc, hs, n)*255);
-plotresult(uint8(pic), result);
+pic = imread('../data/flower.jpg');
+result = myMeanShiftSegmentation(pic, hc, hs, n, 1, 1);
+plotresult(pic, result);
 toc;
 
 
 % *Used Parameters*
-% * Gaussian Kernel bandwidth for color features = 0.06 (For intensities scaled between 0 and 1)
+% * Gaussian Kernel bandwidth for color features = 0.05 (For intensities scaled between 0 and 1)
 % * Gaussian Kernel bandwidth for the spatial features = 500
 % * Number of iterations = 10
 % * Learning rate = 1 (halved after half iterations are completed for better results)
@@ -43,13 +43,13 @@ toc;
 
 %% Bird
 tic;
-hc = 0.1;
-hs = 200;
-n = 15;
+hc = 50;
+hs = 25;
+n = 20;
 
-pic = double(imread('../data/bird.jpg'));
-result = uint8(myMeanShiftSegmentation(pic/255, hc, hs, n)*255);
-plotresult(uint8(pic), result);
+pic = imread('../data/bird.jpg');
+result = myMeanShiftSegmentation(pic, hc, hs, n, 1, 1);
+plotresult(pic, result);
 toc;
 
 
--- assignment-3-segmentation/2/code/myMeanShiftSegmentation.m
+++ assignment-3-segmentation/2/code/myMeanShiftSegmentation.m
@@ -1,9 +1,15 @@
-function [segmented_img] = myMeanShiftSegmentation(img, hc, hs, n)
+function [segmented_img] = myMeanShiftSegmentation(img, hc, hs, n, rescale, lr)
     % img - original image
     % hc - bandwidth parameter for color space
     % hs - spatial bandwidth parameter
     
-    img = imresize(imgaussfilt(img,1), 0.5);
+    if rescale == 1
+        rescale = 255;
+    else
+        rescale = 1;
+    end
+    
+    img = double(imresize(imgaussfilt(img,1), 0.5))/rescale; % smooth, resize and scale to 0-1
     
     [h,w,d] = size(img);	
 	[x,y] = meshgrid(1:w,1:h);
@@ -13,7 +19,7 @@ function [segmented_img] = myMeanShiftSegmentation(img, hc, hs, n)
     full_dim_img(:,:, d+2) = y;
     full_dim_img = double(reshape(full_dim_img, [h*w, d+2]));
     
-    step_size = 1;
+    step_size = lr;
     k = 400; % number of neighbours
      
     for i=1:n
@@ -23,14 +29,17 @@ function [segmented_img] = myMeanShiftSegmentation(img, hc, hs, n)
             full_dim_img(j,:) = full_dim_img(j,:) + step_size*grad;
         end
         
-        progress_pic = uint8(reshape(full_dim_img(:, 1:d), h, w, d)*255);
-        imshow(progress_pic);
+        if i%2==0
+            progress_pic = uint8(reshape(full_dim_img(:, 1:d), h, w, d)*rescale);
+            imshow(progress_pic);
+        end
+        
         if i==floor(n/2)
             step_size = step_size/2;
         end
         waitbar(double(i)/double(n));
     end
-    segmented_img = imresize(reshape(full_dim_img(:, 1:d), h, w, d), 2);
+    segmented_img = uint8(imresize(reshape(full_dim_img(:, 1:d), h, w, d), 2)*rescale);    
 end
 
 function grad = get_grad(pixel, neighbours, hc, hs)
