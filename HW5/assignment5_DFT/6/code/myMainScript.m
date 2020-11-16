22e2fada0808cab00a8417fac1ea86571efff36d
latika24 <latika452000@gmail.com>
2020-11-16 13:39:37 +0530

    qs 6 done final

--- HW5/assignment5_DFT/6/code/myMainScript.m
+++ HW5/assignment5_DFT/6/code/myMainScript.m
@@ -3,12 +3,14 @@
 
 tic;
 %% Your code here
+N = 201;
 k1 = [0 1 0; 1 -4 1 ; 0 1 0];
 filter_1_N = zeros(N);
 filter_1_N( (N-1)/2:(N+3)/2 , (N-1)/2:(N+3)/2 ) = k1;
 fim_1 = fftshift(fft2(filter_1_N));
 log_mag_filter_1 = log(1+abs(fim_1));
 
+figure;
 subplot(1,2,2);
 daspect([1 1 1]); axis tight;
 imshow(log_mag_filter_1, [min(log_mag_filter_1(:)) max(log_mag_filter_1(:))]);  
@@ -16,13 +18,14 @@ colormap('jet'); colorbar;
 h = gca; h.Visible = 'on';
 title('Log Magnitude of the Fourier Transform of Kernel 1')
 
+
 figure;
 s = surf(log_mag_filter_1 ,'FaceAlpha', 0.5); s.EdgeColor = 'none';
 colormap('jet'); colorbar;
 h = gca; h.Visible = 'on';
 title('Surf Image of the Log Magnitude of the Fourier Transform of Kernel 1');
 
-
+%%
 
 k2 = [-1 -1 -1; -1 8 -1 ; -1 -1 -1];
 filter_2_N = zeros(N);
@@ -30,6 +33,7 @@ filter_2_N( (N-1)/2:(N+3)/2 , (N-1)/2:(N+3)/2 ) = k2;
 fim_2 = fftshift(fft2(filter_2_N));
 log_mag_filter_2 = log(1+abs(fim_2));
 
+figure;
 subplot(1,2,2);
 daspect([1 1 1]); axis tight;
 imshow(log_mag_filter_2, [min(log_mag_filter_2(:)) max(log_mag_filter_2(:))]);  
