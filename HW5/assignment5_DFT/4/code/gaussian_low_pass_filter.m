f90133e28455146eee8e75acf5d8666e57135e75
Shubham Lohiya <shubhlohiya.14@gmail.com>
2020-11-16 13:09:13 +0530

    added question 4

---
+++ HW5/assignment5_DFT/4/code/gaussian_low_pass_filter.m
@@ -0,0 +1,22 @@
+function [result, H] = gaussian_low_pass_filter(img, sigma)
+% function to implement gaussian low-pass filter
+
+    [h, w] = size(img);
+%     pad_img=cat(2,zeros([dim,dim/2]),img, zeros([dim,dim/2]));
+%     pad_img=cat(1,zeros([dim/2,2*dim]),pad_img,zeros([dim/2,2*dim]));
+    pad_img = img;
+    imgfft = fft2(pad_img);
+    imgfft_shift = fftshift(imgfft);
+  
+    H = zeros(size(imgfft));
+    for u=1:h
+       for v=1:w
+           if((u-128)^2 + (v-128)^2 <= sigma^2)
+               H(u,v) = exp(-((u-128)^2 + (v-128)^2)/(2*sigma^2));
+           end
+       end
+    end
+    result = ifft2(ifftshift(imgfft_shift.*H));    
+    H = log(abs(H)+1);
+end
+%  Note: Padding gave worse results so I removed it. Padding is more important with convolution in spatial domain 
